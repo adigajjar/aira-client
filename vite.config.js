32e3a82443fec39ada36463194bf77679384805c
Aditya Gajjar <adityanbg25@gmail.com>
2025-04-15 09:22:51 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
   server: {
     proxy: {
       "/api": {
